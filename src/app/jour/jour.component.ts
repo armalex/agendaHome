deb2b1c653a1576daf59cc7a518edb0b6fc76571
armalex <richard.olivier.armalex@gmail.com>
2017-04-21 11:49:18 +0200

    modification modele html

--- src/app/jour/jour.component.ts
+++ src/app/jour/jour.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./jour.component.css']
 })
 export class JourComponent implements OnInit {
+    
+    jourLibelle: String;
+    jourNumero: number;
 
   constructor() { }
 
