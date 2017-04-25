2426ff523fc0f9cfca179cbb0f85f05b350c97ac
armalex <richard.olivier.armalex@gmail.com>
2017-04-25 17:42:29 +0200

    Modification calendrier dynamique

--- src/app/mois/mois.component.ts
+++ src/app/mois/mois.component.ts
@@ -10,13 +10,17 @@ import { MoisService } from '../mois.service';
 export class MoisComponent implements OnInit {
     
 @Input() moisModel: MoisModel;
-@Input() moisIndice:number;
+@Input() moisIndice: number;
 
-@Output() ponyClicked = new EventEmitter<MoisModel>();
-    
+@Output() moisClicked = new EventEmitter<MoisModel>();
+jours: string[]=['','','','1','2','3','4','5','6','7','8','9',
+                '10','11','12','13','14','15','16',
+                '17','18','19','20','21','22','23','24','25',
+                '26','26','27','28','29','30','31'];
+semaines: string[]=['Lu','Ma','Me','Je','Ve','Sa','Di'];
  
   constructor() { }
-
+   
 
   ngOnInit() {
      
@@ -27,7 +31,7 @@ export class MoisComponent implements OnInit {
   }
 
   clicked() {
-    this.ponyClicked.emit(this.moisModel);
+    this.moisClicked.emit(this.moisModel);
   }
   
 }
