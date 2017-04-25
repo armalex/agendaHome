34feeb122afe51f5be3329af3bb1ede8eff95c0f
armalex <richard.olivier.armalex@gmail.com>
2017-04-25 10:15:17 +0200

    Merge branch 'master' of https://github.com/armalex/agendaHome
    # Conflicts:
    #	src/app/mois/mois.component.html

--- src/app/jours.service.ts
+++ src/app/jours.service.ts
@@ -10,19 +10,8 @@ export class JoursService {
 
 
  list():  Observable <Array<JourModel>>{
-    return Observable.of ([
-     {nom:'Janvier',premier:'31',jourDuMois:'Dimanche'},
-     {nom:'Fevrier',nbrJours:28,premierJour:'Mercredi'},
-     {nom:'Mars',nbrJours:31,premierJour:'Mercredi'},
-     {nom:'Avril',nbrJours:30,premierJour:'Samedi'},
-     {nom:'Mai',nbrJours:31,premierJour:'Lundi'},
-     {nom:'Juin',nbrJours:30,premierJour:'Jeudi'},
-     {nom:'Juillet',nbrJours:31,premierJour:'Samedi'},
-     {nom:'Aout',nbrJours:31,premierJour:'Mardi'},
-     {nom:'Septembre',nbrJours:30,premierJour:'Vendredi'},
-     {nom:'Octobre',nbrJours:31,premierJour:'Dimanche'},
-     {nom:'Novembre',nbrJours:30,premierJour:'Mercredi'},
-     {nom:'Décembre',nbrJours:31,premierJour:'Vendredi'},
+    return Observable.of([
+     {nom:'Janvier',premier:true,jourDuMois:2,saint:'st-patrick'}
     ]);
   }
 
